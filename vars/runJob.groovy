a93229937106b1597f162ec5971e8d4e86f97820
donald d <don@hahahardware>
2023-10-31 16:07:01 -0600

    New runJob function

--- vars/runJob.groovy
+++ vars/runJob.groovy
@@ -1,3 +1,10 @@
-def call(String name = 'friend') {
-    echo "Hello, ${name}."
+```groovy
+import hudson.model.*
+import jenkins.model.*
+
+def runJob(jobPath, parameters) {
+    def job = Jenkins.instance.getItemByFullName(jobPath, Job.class)
+    def jobParams = new ParametersAction(parameters.collect { new StringParameterValue(it.key, it.value)})
+    def build = job.scheduleBuild2(0, jobParams)
+    return build
 }
