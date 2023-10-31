d17f8488f887d233f58fc2d8d1aaa625f34b002b
donald d <don@hahahardware>
2023-10-31 16:59:02 -0600

    fixed?

--- vars/runJob.groovy
+++ vars/runJob.groovy
@@ -2,7 +2,7 @@
 import hudson.model.*
 import jenkins.model.*
 
-def runJob(jobPath, parameters) {
+def runJob(String jobPath, Array parameters) {
     def job = Jenkins.instance.getItemByFullName(jobPath, Job.class)
     def jobParams = new ParametersAction(parameters.collect { new StringParameterValue(it.key, it.value)})
     def build = job.scheduleBuild2(0, jobParams)
